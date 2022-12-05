25a8c0ce10d4bf812959c46abd20b7c1bad71bce
michael-mbugua <michael.mbugua@student.moringaschool.com>
2022-12-05 10:45:34 +0300

    initial code

---
+++ air-bnb/src/components/App.js
@@ -0,0 +1,12 @@
+import '../components/App.css';
+import NavBar from './Navbar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
